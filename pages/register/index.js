a33401cb1e7fe01c17e32ee535127bd26b429fc6
Shivamsoni786 <saurabhsoni1101@gmail.com>
2022-11-17 00:14:36 +0530

    register

--- pages/register/index.js
+++ pages/register/index.js
@@ -8,6 +8,7 @@ import { Password } from 'primereact/password';
 import { LayoutContext } from '../../layout/context/layoutcontext';
 import { InputText } from 'primereact/inputtext';
 import { classNames } from 'primereact/utils';
+import Form from 'react-bootstrap/Form';
 
 const RegisterPage = () => {
     const [password, setPassword] = useState('');
@@ -22,7 +23,7 @@ const RegisterPage = () => {
             <div className="flex flex-column align-items-center justify-content-center">
                 {/* <img src={`${contextPath}/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Ebalaji logo" className="mb-4 w-6rem flex-shrink-0" /> */}
                 <div style={{ padding: '0.3rem', background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)' }}>
-                    <div className="w-full surface-card py-5 px-5 sm:px-5" >
+                    <div className="w-full surface-card py-5 px-5 sm:px-5">
                         <div className="text-center mb-5">
                             <img src={`${contextPath}/demo/images/login/avatar.png`} alt="Image" height="50" className="mb-3" />
                             <div className="text-900 text-3xl font-medium mb-3">Welcome, Ebalaji Services</div>
@@ -30,28 +31,93 @@ const RegisterPage = () => {
                         </div>
 
                         <div className="mb-4">
-                            <div></div>
-                            <label htmlFor="email1" className="block text-900 text-xl font-medium mb-1">
-                                Email
-                            </label>
-                            <InputText inputid="email1" type="text" placeholder="Email address" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
-
-                            <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
-                                Password
-                            </label>
-                            <Password inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
+                            <div className="grid">
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">Mobile No.</label>
+                                    <InputText type="text" placeholder="Mobile No." className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }} />
+                                </div>
+                                <div className="col-5">
+                                    <label htmlFor="email1" className="block text-900 text-xl font-medium mb-1">
+                                        Services
+                                    </label>
+                                    <Form.Select aria-label="Default select example" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }}>
+                                        <option>Select Services</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </div>
+                                <div className="col-3">
+                                    <label className="block text-900 text-xl font-medium mb-1">Services Fess</label>
+                                    <InputText type="text" placeholder="Fees" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }} />
+                                </div>
+                            </div>
+                            <div className="grid">
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">Vle Name</label>
+                                    <InputText type="text" placeholder="Vle Name" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }} />
+                                </div>
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">Contact Person</label>
+                                    <InputText type="text" placeholder="Contact person" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }} />
+                                </div>
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">Email</label>
+                                    <InputText type="text" placeholder="Email" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }} />
+                                </div>
+                            </div>
+                            <div className="grid">
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">Pan No.</label>
+                                    <InputText type="text" placeholder="Pan No." className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }} />
+                                </div>
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">Pin Code</label>
+                                    <InputText type="text" placeholder="Pin Code" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }} />
+                                </div>
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">Country</label>
+                                    <Form.Select aria-label="Default select example" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }}>
+                                        <option>Select Country</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </div>
+                            </div>
+                            <div className="grid">
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">State</label>
+                                    <Form.Select aria-label="Default select example" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }}>
+                                        <option>Select State</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </div>
+                                <div className="col-4">
+                                    <label className="block text-900 text-xl font-medium mb-1">City</label>
+                                    <Form.Select aria-label="Default select example" className="w-full md:w-0rem mb-3" style={{ padding: '1rem' }}>
+                                        <option>Select City</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </div>
+                            </div>
 
                             <div className="flex align-items-center justify-content-between mb-5 gap-5">
                                 <div className="flex align-items-center">
                                     <Checkbox inputid="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked)} className="mr-2"></Checkbox>
                                     <label htmlFor="rememberme1">Remember me</label>
                                 </div>
-                                {/* <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
+                                <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
                                     Forgot password?
-                                </a> */}
+                                </a>
                             </div>
                             <Button label="Register" className="w-full p-2 text-xl" onClick={() => router.push('/')}></Button>
                         </div>
+
                         <div className="flex">
                             <span className="text-600 font-medium">Already have an account ?</span>
                             <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }} onClick={() => router.push('/login')}>
