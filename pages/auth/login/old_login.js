5d982cd277e35c67175217d5ca079e9e9692feb0
Namdeo999 <namdeo.madhi999@gmail.com>
2022-11-16 22:25:59 +0530

    update register

--- pages/auth/login/old_login.js
+++ pages/auth/login/old_login.js
@@ -15,24 +15,22 @@ const LoginPage = () => {
     const { layoutConfig } = useContext(LayoutContext);
     const contextPath = getConfig().publicRuntimeConfig.contextPath;
     const router = useRouter();
-    const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
+    const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', {'p-input-filled': layoutConfig.inputStyle === 'filled'});
 
     return (
         <div className={containerClassName}>
             <div className="flex flex-column align-items-center justify-content-center">
-
-                <img src={`${contextPath}/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" />
-                <div style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)' }}>
-                    <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
+                {/* <img src={`${contextPath}/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Ebalaji logo" className="mb-5 w-6rem flex-shrink-0"/> */}
+                <div style={{  padding: '0.3rem', background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)' }}>
+                    <div className="w-full surface-card py-5 px-5 sm:px-5" >
                         <div className="text-center mb-5">
                             <img src={`${contextPath}/demo/images/login/avatar.png`} alt="Image" height="50" className="mb-3" />
-                            <div className="text-900 text-3xl font-medium mb-3">Welcome, EBalaJi</div>
+                            <div className="text-900 text-3xl font-medium mb-3">Welcome, Ebalaji Services</div>
                             <span className="text-600 font-medium">Sign in to continue</span>
                         </div>
 
-                        <div className="mb-5">
-                            <label htmlFor="email1" className="block text-900 text-xl font-medium mb-2">
-
+                        <div>
+                            <label htmlFor="email1" className="block text-900 text-xl font-medium mb-1">
                                 Email
                             </label>
                             <InputText inputid="email1" type="text" placeholder="Email address" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
@@ -40,24 +38,20 @@ const LoginPage = () => {
                             <label htmlFor="password1" className="block text-900 font-medium text-xl mb-1">
                                 Password
                             </label>
-                            <Password inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
+                            <Password inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName='w-full p-3 md:w-30rem'></Password>
 
                             <div className="flex align-items-center justify-content-between mb-5 gap-5">
                                 <div className="flex align-items-center">
                                     <Checkbox inputid="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked)} className="mr-2"></Checkbox>
-                                    <label htmlFor="rememberme1">Remember me</label>
+                                    <label htmlFor="rememberme1">
+                                        Remember me
+                                    </label>
                                 </div>
                                 <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
                                     Forgot password?
                                 </a>
                             </div>
                             <Button label="Login" className="w-full p-2 text-xl " onClick={() => router.push('/')}></Button>
-                        </div>
-                        <div className="flex">
-                            <span className="text-600 font-medium">Don't have an account ?</span>
-                            <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }} onClick={() => router.push('/auth/register')}>
-                                Register Now
-                            </a>
                         </div>
                     </div>
                 </div>
